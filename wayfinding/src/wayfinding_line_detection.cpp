4c27ab7c3df5208070cc385c4ba831cba8c473c8
Nummer42O <johannes.handy007@gmail.com>
2023-01-30 21:16:56 +0100

    split wayfinding source file into line detection and top down projection

---
+++ wayfinding/src/wayfinding_line_detection.cpp
@@ -0,0 +1,156 @@
+#include "wayfinding.hpp"
+
+#include <cmath>
+
+void wayfinding::line_detection::preFilter(cv::InputArray src, cv::OutputArray dst, wayfinding::line_detection::FilterType filter, int ksize) {
+    switch (filter) {
+        case wayfinding::line_detection::FilterType::NO_FILTER:
+            src.copyTo(dst);
+            break;
+        case wayfinding::line_detection::FilterType::MEDIAN:    //Median Filter
+            cv::medianBlur(src, dst, ksize);
+            break;
+        case wayfinding::line_detection::FilterType::NORM_BOX:  //Normalized Box Filter
+            cv::blur(src, dst, cv::Size(ksize, ksize));
+            break;
+        case wayfinding::line_detection::FilterType::BILATERAL: //Bilateral Filter
+            cv::bilateralFilter(src, dst, 5, 200.0, 200.0);
+            break;
+        case wayfinding::line_detection::FilterType::BOX:       //Box Filter
+            cv::boxFilter(src, dst, -1, cv::Size(ksize, ksize));
+            break;
+        case wayfinding::line_detection::FilterType::GAUSS:     //Gaussian Filter
+            cv::GaussianBlur(src, dst, cv::Size(ksize, ksize), 1.5, 2);
+            break;
+    }
+}
+
+void wayfinding::line_detection::getHoughLines(cv::InputArray src, std::vector<cv::Vec4i>& dst, wayfinding::line_detection::parameters_t parameters) {
+    cv::Mat filtered;
+    wayfinding::line_detection::preFilter(src, filtered, parameters.filter_type, parameters.kernel_size);
+
+    wayfinding::line_detection::getHoughLines(filtered, dst, parameters.threshold1, parameters.threshold2, parameters.apertureSize, parameters.rho, parameters.theta, parameters.threshold, parameters.min_line_length, parameters.max_line_gap);
+}
+
+void wayfinding::line_detection::getHoughLines(cv::InputArray src, std::vector<cv::Vec4i>& dst, double threshold1, double threshold2, int apertureSize, double rho, double theta, int threshold, double min_line_length, double max_line_gap) {
+    cv::Mat canny_mat;
+
+    //create contour image
+    cv::Canny(src, canny_mat, threshold1, threshold2, apertureSize);
+    //heuristically identify lines in contour image
+    cv::HoughLinesP(canny_mat, dst, rho, theta, threshold, min_line_length, max_line_gap);
+}
+
+/**
+ * Normalizes the angle of a line (given by dx, dy) to a value between 0 and 90.
+ * 
+ * @param dx: adjacent leg (x_1 - x_0) [px]
+ * @param dy: opposite leg (y_1 - y_0) [px]
+ * 
+ * @return angle [°]
+*/
+double getNormalizedAngle(int dx, int dy) {
+    //get angle and limit it to 0 - 180°
+    double angle = std::abs(std::atan2(dy, dx));
+    if (angle > M_PI_2) { //angles in the range (90, 180] are supposed to be treated like angles in the range (0, -90]
+        angle = M_PI - angle; // pi/2 - (angle - pi/2)
+    }
+
+    return angle;
+}
+
+void wayfinding::line_detection::lineFilter(const std::vector<cv::Vec4i>& src, std::vector<cv::Vec4i>& dst, uint width, double rel_center_width, double angle) {
+    const double rel_threshold = (100.0 - rel_center_width) / 2;
+    const int left_threshold = width * (rel_threshold / 100.0),
+              right_threshold = width * (1 - rel_threshold / 100.0);
+
+    int x_0, y_0,
+        x_1, y_1;
+    int center_x;
+
+    for(cv::Vec4i line: src) {
+        x_0 = line[0]; y_0 = line[1];
+        x_1 = line[2]; y_1 = line[3];
+
+        center_x = (x_0 + x_1) / 2;
+        
+        if ((center_x < left_threshold || center_x > right_threshold) && (::getNormalizedAngle(x_1 - x_0, y_1 - y_0) * 180 / M_PI > angle)) {
+            dst.push_back(line);
+        }
+    }
+}
+
+/**
+ * Calculate a "quality" value for a line (given by angle, length, vertical and horizontal).
+ * (higher means worse)
+ * 
+ * @param angle: line angle relative to the x-axis between 0 and 90 [°]
+ * @param length: line length [px]
+ * @param vertical: distance of the line center from the image top [px]
+ * @param horizontal: distance of the line center from the horizontal image center [px]
+ * @param image_size: size of the source image [px, px]
+ * @param angle_threshold: the optimal angle to achieve; angles smaller or greater get a worse rating [°]
+ * @param vertical_threshold: optimal vertical position to achieve; vertical positions smaller or greater get a worse rating [px]
+ * @param horizontal_threshold: optimal horizontal position to achieve; horizontal positions smaller or greater get a worse rating [px]
+ * 
+ * @return rating value
+*/
+double lineMetric(double angle, double length, double vertical, double horizontal, double angle_threshold, int vertical_threshold, int horizontal_threshold) {
+    double vertical_factor   = 1.0 - std::abs(1.0 - vertical / vertical_threshold),
+           horizontal_factor = 1.0 - std::abs(1.0 - horizontal / horizontal_threshold),
+           angle_factor      = std::abs(1.0 - angle / angle_threshold); //75.0
+
+    return (angle_factor * length) + (horizontal_factor * horizontal) + (vertical_factor * vertical);
+}
+
+double wayfinding::line_detection::imageMetric(const std::vector<cv::Vec4i>& lines, const cv::Size& image_size, double angle_threshold) {
+    const int vertical_threshold   = 2 * image_size.height / 3,
+              horizontal_threshold = image_size.width / 2;
+
+    int x_0, y_0,
+        x_1, y_1;
+    //dx == Ankathete; dy == Gegenkathete
+    int dx, dy, //difference
+        cx, cy; //center
+
+    int vertical;
+    int horizontal;
+    double length;
+    double angle;
+
+    double metric_counter = 0;
+    
+    for (cv::Vec4i line: lines) {
+        x_0 = line[0]; y_0 = line[1];
+        x_1 = line[2]; y_1 = line[3];
+
+        dx = x_1 - x_0; dy = y_1 - y_0;
+        cx = (x_0 + x_1) / 2; cy = (y_0 + y_1) / 2;
+        
+        // vertical    = std::abs(vertical_threshold   - cy);
+        vertical        = cy;
+        // horizontal  = std::abs(horizontal_threshold - cx);
+        if (cx < horizontal_threshold) {
+            horizontal  = cx;
+        } else {
+            horizontal  = image_size.width - cx;
+        }
+        length          = std::sqrt(dx * dx + dy * dy);
+        angle           = ::getNormalizedAngle(dx, dy) * 180 / M_PI; // unit: degrees [°]
+
+        metric_counter += ::lineMetric(angle, length, vertical, horizontal, angle_threshold, vertical_threshold, horizontal_threshold);
+    }
+
+    return metric_counter;
+}
+
+wayfinding::line_detection::parameters_t wayfinding::line_detection::getParametersFromMetric(double metric) {
+    //TODO
+    return wayfinding::line_detection::parameters_t();
+}
+
+void wayfinding::line_detection::drawLines(cv::InputOutputArray img, const std::vector<cv::Vec4i>& lines, const cv::Scalar& color) {
+    for (cv::Vec4i line: lines) {
+        cv::line(img, cv::Point(line[0], line[1]), cv::Point(line[2], line[3]), color);
+    }
+}
---
+++ wayfinding/src/wayfinding_top_down.cpp
@@ -0,0 +1,139 @@
+#include "wayfinding.hpp"
+
+#include <cmath>
+
+cv::Point2d wayfinding::top_down::getVanishingPoint(const cv::Mat& K, double pan, double tilt) {
+    /**
+     * camera rotation matrix:
+     *  R = [r_1, r_2, r_3]
+     *  r_3 = [sin(pan)sin(tilt); cos(tilt); cos(pan)sin(tilt)]
+     * 
+     * v = K * r_3
+     * 
+     *      z
+     *      |
+     *      |  /|
+     * tilt.|_/ |
+     *      |/__|____y
+     * pan./_\  | /
+     *    /   \ |/
+     *   /_____\/
+     * x/
+    */
+
+   cv::Vec3d r_3{std::sin(pan) * std::sin(tilt), std::cos(tilt), std::cos(pan) * std::sin(tilt)};
+
+   cv::Mat vanishing_point = K * cv::Mat(r_3);
+
+   return cv::Point2d(vanishing_point.at<double>(0, 0), vanishing_point.at<double>(1, 0));
+}
+
+bool wayfinding::top_down::getTransformation(cv::Mat& M, std::vector<cv::Point2i>& points, const cv::Size& image_size, const cv::Point2i& vanishing_point, double rel_upper_line_pos) {
+    /** Concept:
+     * unkown: x
+     * known: A, B, y
+     * 
+     * A -> B ?= [x, y]
+     * A + r * (B - A) ?= [x, y]
+     * 
+     * yA + r * (yB - yA) = y
+     * r = (y - yA) / (yB - yA)
+     * x = xA + r * (xB - xA)
+     * 
+     * here:
+     *  A = bottom_left/bottom_right
+     *  B = vanishing_point
+     */
+
+    //move bottom left and bottom right further out of the window to cover a wider window
+    const int offset = 3 * vanishing_point.x;
+    const cv::Point2i bottom_left(0 - offset, image_size.height),
+                      bottom_right(image_size.width + offset, image_size.height);
+
+    //assumes the vanishing point is close enough so this still lies within the image
+    const double y = vanishing_point.y + (image_size.height - vanishing_point.y) * rel_upper_line_pos;
+    const double y_rounded = std::round(y);
+
+    //left line
+    double r_left = (y - bottom_left.y) / (vanishing_point.y - bottom_left.y);
+    cv::Point2i top_left(std::round(bottom_left.x + r_left * (vanishing_point.x - bottom_left.x)), y_rounded);
+
+    //right line
+    double r_right = (y - bottom_right.y) / (vanishing_point.y - bottom_right.y);
+    cv::Point2i top_right(std::round(bottom_right.x + r_right * (vanishing_point.x - bottom_right.x)), y_rounded);
+
+    points = std::vector<cv::Point2i>{top_left, top_right, bottom_left, bottom_right};
+
+
+    //get transformation matrix
+    M = cv::findHomography(
+        points,
+        std::vector<cv::Point2i>{
+            cv::Point2i(0, 0), cv::Point2i(image_size.width, 0),
+            cv::Point2i(0, image_size.height), cv::Point2i(image_size.width, image_size.height)
+        }
+    );
+    
+    return !M.empty();
+}
+
+bool wayfinding::top_down::getTransformation(cv::Mat& M, const cv::Size& image_size, const cv::Point2i& vanishing_point, double rel_upper_line_pos) {
+    //move bottom left and bottom right further out of the window to cover a wider window
+    const int offset = 2 * vanishing_point.x;
+    const cv::Point2i bottom_left(0 - offset, image_size.height),
+                      bottom_right(image_size.width + offset, image_size.height);
+
+    //assumes the vanishing point is close enough so this still lies within the image
+    const double y = vanishing_point.y + (image_size.height - vanishing_point.y) * rel_upper_line_pos;
+    const double y_rounded = std::round(y);
+
+    //left line
+    const double r_left = (y - bottom_left.y) / (vanishing_point.y - bottom_left.y);
+    cv::Point2i top_left(std::round(bottom_left.x + r_left * (vanishing_point.x - bottom_left.x)), y_rounded);
+
+    //right line
+    const double r_right = (y - bottom_right.y) / (vanishing_point.y - bottom_right.y);
+    cv::Point2i top_right(std::round(bottom_right.x + r_right * (vanishing_point.x - bottom_right.x)), y_rounded);
+
+
+    //get transformation matrix
+    M = cv::findHomography(
+        std::vector<cv::Point2i>{
+            top_left,       top_right,
+            bottom_left,    bottom_right
+        },
+        std::vector<cv::Point2i>{
+            cv::Point2i(0, 0),                  cv::Point2i(image_size.width, 0),
+            cv::Point2i(0, image_size.height),  cv::Point2i(image_size.width, image_size.height)
+        }
+    );
+    
+    return !M.empty();
+}
+
+void wayfinding::top_down::drawVanishingLines(cv::InputOutputArray img, const cv::Point2i& vanishing_point, const std::vector<cv::Point2i>& trapeze) {
+    const cv::Scalar red(0, 0, 255),
+                     green(0, 255, 0);
+    cv::Point2i top_left = trapeze[0],    top_right = trapeze[1],
+                bottom_left = trapeze[2], bottom_right = trapeze[3];
+
+    cv::line(img, bottom_left,  vanishing_point, red, 3);
+    cv::line(img, bottom_right, vanishing_point, red, 3);
+
+    cv::line(img, bottom_left,  bottom_right,   green, 3);
+    cv::line(img, bottom_left,  top_left,       green, 3);
+    cv::line(img, bottom_right, top_right,      green, 3);
+    cv::line(img, top_left,     top_right,      green, 3);
+}
+
+void wayfinding::top_down::transformToTopDown(cv::InputArray src, cv::OutputArray dst, cv::InputArray M, const cv::Size& dst_size) {
+    cv::warpPerspective(src, dst, M, dst_size, cv::INTER_NEAREST);
+}
+
+cv::Point2i wayfinding::top_down::unwarpPoint(const cv::Mat& transformation_matrix, const cv::Point2i& point) {
+    cv::Vec3d warped_point{static_cast<double>(point.x), static_cast<double>(point.y), 1.0};
+    cv::Mat normal_point = transformation_matrix.inv() * warped_point;
+
+    double denominator = normal_point.at<double>(2, 0);
+    return cv::Point2i(normal_point.at<double>(0, 0) / denominator, normal_point.at<double>(1, 0) / denominator);
+}
