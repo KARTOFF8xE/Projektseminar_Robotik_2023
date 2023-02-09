a8b01f3153a513ca7bb33d7f6a905ca81cc09322
gmuck <georg.muck@student.tu-freiberg.de>
2023-02-09 19:25:58 +0100

    vp: Weiter geschrieben (launch fehlt noch)

--- visualize_path/src/node.cpp
+++ visualize_path/src/node.cpp
@@ -10,11 +10,11 @@
 using std::placeholders::_1;
 
 Node::Node(): rclcpp::Node("visualize_path") {
-    this->declare_parameter<std::string>("topics.dmc.laserscan",            "/sick/scan");
-    this->declare_parameter<double> ("robot_specific.wheel_inside",         .2854);             // Distance of the vertical Plane in the center of the Robot to the verical inside Plane of the Wheels
+    this->declare_parameter<std::string>("topics.sub_topic",                "/lidar_path_width");
+    this->declare_parameter<double> ("robot_specific.wheel_inside",         .2854);             // Distance of the vertical Plane in the center of the Robot to the vertical inside Plane of the Wheels
     this->declare_parameter<double> ("robot_specific.wheel_width",          .1143);             // Width of the Wheels
     
-    this->get_parameter("topics.dmc.laserscan",                 custom_parameters.sub_topic);
+    this->get_parameter("topics.sub_topic",                     custom_parameters.sub_topic);
     this->get_parameter("robot_specific.wheel_inside",          custom_parameters.wheel_inside);
     this->get_parameter("robot_specific.wheel_width",           custom_parameters.wheel_width);
 
@@ -24,6 +24,12 @@ Node::Node(): rclcpp::Node("visualize_path") {
 void Node::callback(const custom_msgs::msg::Distance::SharedPtr msg) {
     rclcpp::Logger logger = this->get_logger();
 
-    visualize_path::visualize_street_view(limits_vec, custom_parameters.wheel_inside, custom_parameters.wheel_width);
+    filters::limit limit {
+        msg->header.stamp,
+        msg->left,
+        msg->right
+    };
 
+    limits_vec.push_back(limit);
+    visualize_path::visualize_street_view(limits_vec, custom_parameters.wheel_inside, custom_parameters.wheel_width);
 }
