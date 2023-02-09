7f78d1e66ef0e12c6f66d709de0415c65f417a24
Nummer42O <johannes.handy007@gmail.com>
2023-02-09 11:02:47 +0100

    lcd: fixed launch file to also launch with no visualization

--- lidar_curb_detection/launch/lidar_curb_detection.launch.py
+++ lidar_curb_detection/launch/lidar_curb_detection.launch.py
@@ -4,15 +4,16 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
-from launch.conditions import IfCondition
+from launch.conditions import IfCondition, UnlessCondition
 from launch.actions import DeclareLaunchArgument
 
 PKG_NAME = "lidar_curb_detection";
 
 def generate_launch_description():
-    param_path = LaunchConfiguration("lidar_curb_detection_param_dir", default=os.path.join(get_package_share_directory(PKG_NAME), "param/param.yaml"))
+    do_visualize = LaunchConfiguration("visualize");
     log_level = LaunchConfiguration("log_level");
-    do_visualize = LaunchConfiguration("visualize")
+
+    param_path = LaunchConfiguration("lidar_curb_detection_param_dir", default=os.path.join(get_package_share_directory(PKG_NAME), "param/param.yaml"));
 
     ld = LaunchDescription(
         (
@@ -39,6 +40,21 @@ def generate_launch_description():
             parameters=[param_path],
             arguments=[
                 "-v",
+                "--ros-args", "--log-level", log_level
+            ],
+
+            output="screen",
+            emulate_tty=True,
+            shell=True
+        )
+    );
+    ld.add_action(
+        Node(
+            condition=UnlessCondition(do_visualize),
+            package="lidar_curb_detection",
+            executable="lidar_curb_detection",
+            parameters=[param_path],
+            arguments=[
                 "--ros-args", "--log-level", log_level
             ],
 
--- lidar_curb_detection/launch/lidar_curb_detection.sublaunch.launch.py
+++ lidar_curb_detection/launch/lidar_curb_detection.sublaunch.launch.py
@@ -9,12 +9,19 @@ from launch.actions import DeclareLaunchArgument
 PKG_NAME = "lidar_curb_detection";
 
 def generate_launch_description():
+    do_visualize = LaunchConfiguration("visualize");
     log_level = LaunchConfiguration("log_level");
-
+    
     param_path = os.path.join(get_package_share_directory(PKG_NAME), "param/param.yaml");
 
     ld = LaunchDescription(
         (
+            DeclareLaunchArgument(
+                "visualize",
+                default_value='False',
+                choices=["True", "False"],
+                description="Visualize 2D-Pointcloud and Limits"
+            ),
             DeclareLaunchArgument(
                 "log_level",
                 default_value='info',
